0dc9fe31c7576d12915f70768bbb04aaa49bd39e
ilnar861 <ilnarkino@yandex.ru>
2025-09-10 21:08:55 +0300

    Added floating action button

--- app/src/main/java/ru/ilnarkin/ilnarapp/ui/screens/NotesScreen.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/screens/NotesScreen.kt
@@ -1,14 +1,26 @@
 package ru.ilnarkin.ilnarapp.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import ru.ilnarkin.ilnarapp.R
 import ru.ilnarkin.ilnarapp.models.Note
 import ru.ilnarkin.ilnarapp.models.NoteType
 import ru.ilnarkin.ilnarapp.ui.components.LoadButtonComponent
@@ -20,7 +32,7 @@ fun NotesScreen() {
 
 	val notes = getNotesList()
 
-	Column {
+	Box(Modifier.fillMaxSize()) {
 		LazyColumn(contentPadding = PaddingValues(top = 30.dp, bottom = 60.dp)) {
 			item {
 				Row(Modifier.padding(bottom = 15.dp)) {
@@ -36,7 +48,18 @@ fun NotesScreen() {
 				}
 			}
 		}
+
+		FloatingActionButton(
+			containerColor = colorResource(R.color.primary_color),
+			contentColor = Color.White,
+			shape = CircleShape,
+			modifier = Modifier.align(Alignment.BottomEnd)
+				.absolutePadding(bottom=30.dp, right=30.dp).background(Color.Transparent),
+			onClick = {  }) {
+			Icon(painter = painterResource(R.drawable.ic_plus), contentDescription = "Добавить")
+		}
 	}
+
 }
 
 fun getNotesList() : MutableList<Note>{
