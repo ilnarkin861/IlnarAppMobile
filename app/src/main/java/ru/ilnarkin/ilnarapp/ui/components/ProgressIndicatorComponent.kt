6cab2f5c1c93d263abe4990f2d7597ec225782f8
ilnar861 <ilnarkino@yandex.ru>
2025-09-18 19:30:17 +0300

    Created progress indicator component

---
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/ProgressIndicatorComponent.kt
@@ -0,0 +1,22 @@
+package ru.ilnarkin.ilnarapp.ui.components
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import ru.ilnarkin.ilnarapp.R
+
+
+@Composable
+fun ProgressIndicatorComponent(size: Int) {
+	CircularProgressIndicator(
+		modifier = Modifier.width(size.dp).height(size.dp),
+		strokeWidth = 3.dp,
+		color = colorResource(R.color.primary_color),
+		trackColor = MaterialTheme.colorScheme.surfaceVariant,
+	)
+}
