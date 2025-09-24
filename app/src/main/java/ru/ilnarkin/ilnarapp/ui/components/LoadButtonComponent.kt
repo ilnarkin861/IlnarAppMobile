ebb60874b18d9d3636d85b42f8d3fd407200df17
ilnar861 <ilnarkino@yandex.ru>
2025-09-24 18:25:42 +0300

    Removed unused imports

--- app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/LoadButtonComponent.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/LoadButtonComponent.kt
@@ -14,11 +14,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
-import ru.ilnarkin.ilnarapp.R
 import ru.ilnarkin.ilnarapp.helpers.getInterFont
 
 
