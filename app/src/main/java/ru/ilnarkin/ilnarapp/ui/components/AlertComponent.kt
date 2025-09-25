f6004c00d1bbe7a4ae726f3acdeb30004d016992
ilnar861 <ilnarkino@yandex.ru>
2025-09-25 18:50:14 +0300

    Changed buttons

--- app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/AlertComponent.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/AlertComponent.kt
@@ -1,6 +1,8 @@
 package ru.ilnarkin.ilnarapp.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,11 +13,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,31 +35,19 @@ fun AlertComponent(
 	showed: Boolean = false,
 	action: () -> Unit
 ) {
+	val interactionSource = remember { MutableInteractionSource() }
 	val dialogState = rememberMaterialDialogState()
+	val fontFamily = getInterFont()
+
 	if (showed) dialogState.show()
+
 
 	MaterialDialog(
 		dialogState = dialogState,
 		shape = MaterialTheme.shapes.small,
 		onCloseRequest = { MaterialDialogState.Saver() },
-		buttons = {
-			positiveButton(
-				text = "Понятно",
-				textStyle = TextStyle(
-					color = colorResource(R.color.title_color),
-					fontFamily = getInterFont(),
-					fontWeight = FontWeight.SemiBold),
-				onClick = {
-					dialogState.hide()
-					action()
-				}
-			)
-		}
 	) {
-		Column(
-			modifier = Modifier.background(Color.White),
-
-			) {
+		Column(modifier = Modifier.background(Color.White)) {
 			Row(
 				modifier = Modifier.fillMaxWidth().padding(top = 15.dp, bottom = 10.dp),
 				horizontalArrangement = Arrangement.Center
@@ -78,21 +68,40 @@ fun AlertComponent(
 			}
 
 			Row(
-				modifier = Modifier.fillMaxWidth().padding(start = 10.dp, end = 10.dp, bottom = 30.dp),
+				modifier = Modifier.fillMaxWidth().padding(start = 10.dp, end = 10.dp, bottom = 25.dp),
 				horizontalArrangement = Arrangement.Center
 			) {
 				Text(
 					text = message,
 					fontWeight = FontWeight.Bold,
-					fontSize = 16.sp,
-					fontFamily = getInterFont(),
+					fontSize = 18.sp,
+					fontFamily = fontFamily,
 
 					color = if (!success) colorResource(R.color.danger_color)
 						else colorResource(R.color.primary_color)
 				)
 			}
+
+			Row(
+				modifier = Modifier.fillMaxWidth().padding(top = 30.dp, bottom = 30.dp),
+				horizontalArrangement = Arrangement.Center
+			) {
+				Text(
+					modifier = Modifier.clickable(
+						interactionSource = interactionSource,
+						indication = null,
+						onClick = {
+							dialogState.hide()
+							action()
+						}
+					),
+					text = "Понятно",
+					fontWeight = FontWeight.SemiBold,
+					fontSize = 15.sp,
+					color = Color.Gray,
+					fontFamily = fontFamily,
+				)
+			}
 		}
-
 	}
-
 }
--- app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/ConfirmComponent.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/ConfirmComponent.kt
@@ -1,6 +1,8 @@
 package ru.ilnarkin.ilnarapp.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,11 +13,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,46 +31,20 @@ import ru.ilnarkin.ilnarapp.helpers.getInterFont
 @Composable
 fun ConfirmComponent(showed: Boolean = false, action: (confirmed: Boolean) -> Unit) {
 
+	val interactionSource = remember { MutableInteractionSource() }
 	val dialogState = rememberMaterialDialogState()
+	val fontFamily = getInterFont()
+
 	if (showed) dialogState.show()
 
 	MaterialDialog(
 		dialogState = dialogState,
 		shape = MaterialTheme.shapes.small,
 		onCloseRequest = { MaterialDialogState.Saver() },
-		buttons = {
-			positiveButton(
-				text = "Да",
-				textStyle = TextStyle(
-					color = colorResource(R.color.danger_color),
-					fontFamily = getInterFont(),
-					fontWeight = FontWeight.SemiBold),
-				onClick = {
-					dialogState.hide()
-					action(true)
-				}
-			)
-
-			negativeButton(
-				text = "Нет",
-				textStyle = TextStyle(
-					color = colorResource(R.color.title_color),
-					fontFamily = getInterFont(),
-					fontWeight = FontWeight.SemiBold),
-				onClick = {
-					dialogState.hide()
-					action(false)
-				}
-			)
-
-		}
 	) {
-		Column(
-			modifier = Modifier.background(Color.White),
-
-			) {
+		Column(modifier = Modifier.background(Color.White).padding(20.dp)) {
 			Row(
-				modifier = Modifier.fillMaxWidth().padding(top = 15.dp, bottom = 10.dp),
+				modifier = Modifier.fillMaxWidth().padding(top = 10.dp, bottom = 10.dp),
 				horizontalArrangement = Arrangement.Center
 			) {
 				Icon(
@@ -79,19 +55,56 @@ fun ConfirmComponent(showed: Boolean = false, action: (confirmed: Boolean) -> Un
 			}
 
 			Row(
-				modifier = Modifier.fillMaxWidth().padding(start = 10.dp, end = 10.dp, bottom = 30.dp),
+				modifier = Modifier.fillMaxWidth().padding(start = 10.dp, end = 10.dp, bottom = 25.dp),
 				horizontalArrangement = Arrangement.Center
 			) {
 				Text(
 					text = "Точно хочешь удалить?",
 					fontWeight = FontWeight.Bold,
-					fontSize = 16.sp,
-					fontFamily = getInterFont(),
+					fontSize = 18.sp,
+					fontFamily = fontFamily,
 					color = colorResource(R.color.warning_color)
 				)
 			}
+
+			Row(
+				modifier = Modifier.fillMaxWidth().padding(top = 30.dp, bottom = 15.dp),
+				horizontalArrangement = Arrangement.Center
+			) {
+				Row {
+					Text(
+						modifier = Modifier.padding(horizontal = 15.dp).clickable(
+							interactionSource = interactionSource,
+							indication = null,
+							onClick = {
+								dialogState.hide()
+								action(false)
+							}
+						),
+						text = "Нет",
+						fontWeight = FontWeight.SemiBold,
+						fontSize = 15.sp,
+						color = Color.Gray,
+						fontFamily = fontFamily,
+					)
+
+					Text(
+						modifier = Modifier.padding(horizontal = 15.dp).clickable(
+							interactionSource = interactionSource,
+							indication = null,
+							onClick = {
+								dialogState.hide()
+								action(true)
+							}
+						),
+						text = "Да",
+						fontWeight = FontWeight.SemiBold,
+						fontSize = 15.sp,
+						color = colorResource(R.color.danger_color),
+						fontFamily = fontFamily,
+					)
+				}
+			}
 		}
-
 	}
-
 }
