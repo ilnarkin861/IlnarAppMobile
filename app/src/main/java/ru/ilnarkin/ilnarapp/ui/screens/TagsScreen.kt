a3e490ff700fce5754778eaa92493b0e7803e960
ilnarkin861 <ilnarkino@yandex.ru>
2025-10-23 17:04:24 +0300

    Implemented tags list

--- app/src/main/java/ru/ilnarkin/ilnarapp/ui/screens/TagsScreen.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/screens/TagsScreen.kt
@@ -1,15 +1,102 @@
 package ru.ilnarkin.ilnarapp.ui.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Devices.PIXEL_3
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import ru.ilnarkin.ilnarapp.R
+import ru.ilnarkin.ilnarapp.ui.components.ListItemComponent
+import ru.ilnarkin.ilnarapp.ui.components.LoadButtonComponent
+import ru.ilnarkin.ilnarapp.ui.components.ProgressIndicatorComponent
 
 
 @Composable
+@Preview(showBackground = true, showSystemUi = true, device = PIXEL_3)
 fun TagsScreen() {
-	Column(Modifier.fillMaxSize()) {
-		Text("Tags screen")
+
+	val tags = getTags(20)
+	var loading by remember { mutableStateOf(false) }
+	val listState = rememberLazyListState()
+
+
+	LaunchedEffect(Unit) {
+		loading = true
+
+		delay(1500)
+
+		loading = false
+	}
+
+
+	Box(Modifier.fillMaxSize()) {
+
+		if (loading){
+			Box(
+				modifier = Modifier.fillMaxSize(),
+				contentAlignment = Alignment.Center){
+				ProgressIndicatorComponent(60)
+			}
+		}
+
+		if (!loading && !tags.isEmpty()){
+			LazyColumn(
+				state = listState,
+				contentPadding = PaddingValues(top = 30.dp, bottom = 60.dp)
+			) {
+				item {
+					Row(Modifier.padding(bottom = 25.dp)) {
+						LoadButtonComponent(nextButton = false, action = { delay(1500) })
+					}
+				}
+
+				itemsIndexed(tags){index, tag ->
+
+					Row(Modifier.fillMaxWidth().padding(vertical = 5.dp)) {
+						ListItemComponent(
+							tag.id,
+							tag.title,
+							editAction = {},
+							deleteAction = {})
+					}
+
+					if (index != tags.count() -1){
+						HorizontalDivider(thickness = 1.dp, color = colorResource(R.color.border_color))
+					}
+				}
+
+				item {
+					Row(Modifier.padding(top = 25.dp, bottom = 30.dp)) {
+						LoadButtonComponent(action = {
+							delay(1500)
+							listState.scrollToItem(0)
+						})
+					}
+				}
+			}
+		}
+
+
+
+
 	}
 }
+
