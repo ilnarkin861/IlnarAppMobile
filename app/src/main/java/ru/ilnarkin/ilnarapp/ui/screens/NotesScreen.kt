45507da65e55a00347a54b11dba5e376e33257fa
ilnarkin861 <ilnarkino@yandex.ru>
2025-10-23 17:03:25 +0300

    Moved functions to func.kt file

--- app/src/main/java/ru/ilnarkin/ilnarapp/ui/screens/NotesScreen.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/screens/NotesScreen.kt
@@ -43,10 +43,7 @@ import kotlinx.coroutines.launch
 import ru.ilnarkin.ilnarapp.R
 import ru.ilnarkin.ilnarapp.enums.ActionType
 import ru.ilnarkin.ilnarapp.helpers.getInterFont
-import ru.ilnarkin.ilnarapp.models.Archive
 import ru.ilnarkin.ilnarapp.models.Note
-import ru.ilnarkin.ilnarapp.models.NoteType
-import ru.ilnarkin.ilnarapp.models.Tag
 import ru.ilnarkin.ilnarapp.ui.components.AlertComponent
 import ru.ilnarkin.ilnarapp.ui.components.LoadButtonComponent
 import ru.ilnarkin.ilnarapp.ui.components.NoteDetailsComponent
@@ -292,59 +289,4 @@ fun NotesScreen() {
 			}
 		}
 	}
-}
-
-
-fun getNotesList() : MutableList<Note>{
-	val notes = mutableListOf<Note>()
-	val noteTitle = "Событие: Фестиваль дружбы"
-	val noteText = """
-		В маленьком городке, расположенном у подножия гор, ежегодно проходит фестиваль дружбы. Это событие собирает людей из разных уголков региона, и каждый год его темы отличаются.
-	""".trimIndent()
-	val noteDate = "2023-10-20"
-	val noteType = NoteType(id = "", title = "Заметка")
-
-	val tags = getTags(5)
-
-	for (i in 1..10){
-		notes.add(Note(
-			title = "$noteTitle - $i",
-			text = noteText,
-			date = noteDate,
-			noteType =  noteType,
-			tags = tags,
-			archive = Archive(id = "", title = "Архив 1")
-		))
-	}
-
-	return notes
-}
-
-fun getNoteTypes(): List<NoteType>{
-	val noteTypes = mutableListOf<NoteType>()
-
-	noteTypes.add(NoteType(id = "", title = "Событие"))
-	noteTypes.add(NoteType(id = "", title = "Заметка"))
-
-	return noteTypes
-}
-
-fun getArchives(): List<Archive>{
-	val archives = mutableListOf<Archive>()
-
-	for (i in 1..5){
-		archives.add(Archive(id = "", title = "Архив ${i}"))
-	}
-
-	return archives
-}
-
-fun getTags(count: Int): MutableList<Tag>{
-	val tags = mutableListOf<Tag>()
-
-	for (i in 1..count){
-		tags.add(Tag(id = "", title = "Тег ${i}"))
-	}
-
-	return tags
 }
