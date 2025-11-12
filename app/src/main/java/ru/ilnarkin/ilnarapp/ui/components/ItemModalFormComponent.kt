58e1326d4cd8920a09b78666fb2c4b3cf611a9a1
ilnarkin861 <ilnarkino@yandex.ru>
2025-11-12 18:55:11 +0300

    Implemented item modal form component

---
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/ItemModalFormComponent.kt
@@ -0,0 +1,190 @@
+package ru.ilnarkin.ilnarapp.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.vanpra.composematerialdialogs.rememberMaterialDialogState
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import ru.ilnarkin.ilnarapp.R
+import ru.ilnarkin.ilnarapp.helpers.getInterFont
+
+
+@Composable
+fun ItemModalFormComponent(
+	itemText: String?,
+	label: String,
+	showed: Boolean = false,
+	action: suspend (text: String) -> Unit,
+	close: () -> Unit
+) {
+	val fontFamily = getInterFont()
+	
+	val mutableItemText = remember { mutableStateOf(itemText?: "") }
+
+	var itemTextIsError by remember { mutableStateOf(false) }
+
+	val interactionSource = remember { MutableInteractionSource() }
+
+	val scope = rememberCoroutineScope()
+
+	var saving by remember { mutableStateOf(false) }
+
+	val dialogState = rememberMaterialDialogState()
+
+	if (showed) dialogState.show()
+
+
+	Column(modifier = Modifier.background(Color.White)) {
+		Column(
+			modifier = Modifier.fillMaxWidth()
+				.padding(top = 30.dp, start = 15.dp, end = 15.dp, bottom = 40.dp)
+		){
+			Row(
+				modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp)
+			){
+				Text(
+					color = colorResource(R.color.title_color),
+					text = label,
+					fontFamily = fontFamily,
+					fontSize = 18.sp,
+					fontWeight = FontWeight.Bold
+				)
+			}
+
+			Row(
+				modifier = Modifier.fillMaxWidth()
+			){
+				OutlinedTextField(
+					modifier = Modifier.fillMaxWidth(),
+					textStyle = TextStyle(
+						fontFamily = fontFamily,
+						fontSize = 15.sp,
+					),
+					value = mutableItemText.value,
+					singleLine = true,
+					isError = itemTextIsError,
+					label = { Text(label) },
+					onValueChange = {text ->
+						mutableItemText.value = text
+						itemTextIsError = mutableItemText.value.isEmpty()},
+					colors = OutlinedTextFieldDefaults.colors(
+						unfocusedBorderColor = colorResource(R.color.inputs_border_color),
+						focusedBorderColor = colorResource(R.color.primary_color),
+						unfocusedLabelColor = colorResource(R.color.inputs_placeholder_color),
+						focusedLabelColor = colorResource(R.color.primary_color),
+						focusedTextColor = colorResource(R.color.text_color),
+						unfocusedTextColor = colorResource(R.color.text_color)
+					),
+					shape = RoundedCornerShape(10.dp))
+			}
+
+			if (itemTextIsError){
+				Row(modifier = Modifier.padding(top = 5.dp, bottom = 10.dp)) {
+					Text(
+						text = "Обязательное поле",
+						color = colorResource(R.color.danger_color),
+						fontFamily = fontFamily,
+						fontSize = 13.sp
+					)
+				}
+			}
+
+			Row(
+				modifier = Modifier.fillMaxWidth().padding(top = 30.dp)
+			){
+				Button(
+					modifier = Modifier.fillMaxWidth().height(60.dp),
+					enabled = !saving,
+					shape = RoundedCornerShape(10.dp),
+					colors = ButtonDefaults.buttonColors(
+						containerColor = colorResource(R.color.primary_color),
+						disabledContainerColor = colorResource(R.color.primary_color).copy(alpha = 0.8f)),
+					onClick = {
+						itemTextIsError = mutableItemText.value.isEmpty()
+
+						if(!itemTextIsError){
+							saving = true
+
+							scope.launch {
+								scope.async {
+									action(mutableItemText.value) }.await()
+							}.invokeOnCompletion { saving = false }
+						}
+					}
+				) {
+					if (saving){
+						Row(modifier = Modifier.fillMaxSize(),
+							horizontalArrangement = Arrangement.Center,
+							verticalAlignment = Alignment.CenterVertically) {
+							CircularProgressIndicator(
+								modifier = Modifier.size(20.dp),
+								strokeWidth = 2.dp,
+								color = Color.White
+							)
+						}
+					}
+					else{
+						Text(
+							text = "Сохранить",
+							fontFamily = fontFamily,
+							fontSize = 16.sp,
+							fontWeight = FontWeight.SemiBold
+						)
+					}
+				}
+			}
+
+
+			Row(
+				modifier = Modifier.fillMaxWidth().padding(top = 25.dp),
+				horizontalArrangement = Arrangement.Center
+			) {
+				Text(
+					modifier = Modifier.clickable(
+						interactionSource = interactionSource,
+						indication = null,
+						onClick = {
+							dialogState.hide()
+							close()
+						}
+					),
+					text = "Закрыть",
+					fontWeight = FontWeight.SemiBold,
+					fontSize = 15.sp,
+					color = Color.Gray,
+					fontFamily = fontFamily,
+				)
+			}
+		}
+	}
+}
