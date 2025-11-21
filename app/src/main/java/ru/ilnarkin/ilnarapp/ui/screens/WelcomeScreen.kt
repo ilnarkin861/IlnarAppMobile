10433b9065780933e5961d0859caf1db616764dc
ilnarkin861 <ilnarkino@yandex.ru>
2025-11-21 18:43:33 +0300

    Implemented Welcome Screen

---
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/screens/WelcomeScreen.kt
@@ -0,0 +1,39 @@
+package ru.ilnarkin.ilnarapp.ui.screens
+
+import android.content.Intent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
+import ru.ilnarkin.ilnarapp.MainActivity
+import ru.ilnarkin.ilnarapp.R
+
+@Composable
+fun WelcomeScreen() {
+
+	val context = LocalContext.current
+	val intent = Intent(context, MainActivity::class.java)
+
+	LaunchedEffect(Unit) {
+		delay(2000)
+		context.startActivity(intent)
+	}
+
+	Box(modifier = Modifier
+		.fillMaxSize()
+		.background(colorResource(R.color.primary_color)),
+		contentAlignment = Alignment.Center){
+
+		Image(
+			painter = painterResource(R.drawable.ilnarapp_logo),
+			contentDescription = "Logo")
+	}
+}
