8d247d797228f54dfec2bff165f1d0e65eeaf981
ilnarkin861 <ilnarkino@yandex.ru>
2025-11-21 11:47:55 +0300

    Refactoring

--- app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/MessageComponent.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/MessageComponent.kt
@@ -2,7 +2,6 @@ package ru.ilnarkin.ilnarapp.ui.components
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.sp
 import ru.ilnarkin.ilnarapp.R
