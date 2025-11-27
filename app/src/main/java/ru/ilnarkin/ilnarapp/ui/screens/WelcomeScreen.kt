a28af3976722f6ecf6f7a458fcfdaa9d88feace0
ilnarkin861 <ilnarkino@yandex.ru>
2025-11-27 19:18:11 +0300

    Refactoring

--- app/src/main/java/ru/ilnarkin/ilnarapp/ui/screens/WelcomeScreen.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/screens/WelcomeScreen.kt
@@ -1,5 +1,6 @@
 package ru.ilnarkin.ilnarapp.ui.screens
 
+import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -8,29 +9,43 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import kotlinx.coroutines.delay
 import ru.ilnarkin.ilnarapp.R
+import ru.ilnarkin.ilnarapp.helpers.KEY_TOKEN
+import ru.ilnarkin.ilnarapp.helpers.PREFS_NAME
 import ru.ilnarkin.ilnarapp.routes.NavRoutes
 
 
 @Composable
 fun WelcomeScreen(navController: NavController) {
 
+	val context = LocalContext.current
+	val sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+
 	LaunchedEffect(Unit) {
 		delay(2000)
-		/*navController.navigate(NavRoutes.LoginScreen.route){
-			popUpTo(navController.graph.findStartDestination().id) {
-				inclusive = true
-			}
-		}*/
 
-		navController.navigate(NavRoutes.PinLockScreen.route){
-			popUpTo(navController.graph.findStartDestination().id) {
-				inclusive = true
+		val token = sharedPreferences.getString(KEY_TOKEN, null)
+
+		if (token == null){
+			navController.navigate(NavRoutes.LoginScreen.route){
+				popUpTo(navController.graph.findStartDestination().id) {
+					inclusive = true
+				}
+			}
+		}
+
+		else{
+			navController.navigate(NavRoutes.PinLockScreen.route){
+				popUpTo(navController.graph.findStartDestination().id) {
+					inclusive = true
+				}
 			}
 		}
 	}
