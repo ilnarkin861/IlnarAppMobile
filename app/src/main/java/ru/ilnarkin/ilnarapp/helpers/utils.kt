cbd90e3f44c37831ad96dadb970fb2753cba43ca
ilnarkin861 <ilnarkino@yandex.ru>
2025-11-27 10:59:35 +0300

    Created utils file

---
+++ app/src/main/java/ru/ilnarkin/ilnarapp/helpers/utils.kt
@@ -0,0 +1,27 @@
+package ru.ilnarkin.ilnarapp.helpers
+
+import android.util.Patterns
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import ru.ilnarkin.ilnarapp.R
+
+
+const val PREFS_NAME = "AppSettings"
+const val KEY_PIN = "pin"
+const val KEY_IS_LOGGED_IN = "isLoggedIn"
+
+
+fun getInterFont(): FontFamily{
+	val provider = GoogleFont.Provider(
+		providerAuthority = "com.google.android.gms.fonts",
+		providerPackage = "com.google.android.gms",
+		certificates = R.array.com_google_android_gms_fonts_certs
+	)
+
+	return FontFamily(Font(googleFont = GoogleFont("Inter"), provider))
+}
+
+fun validEmail(email: String): Boolean{
+	return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+}
