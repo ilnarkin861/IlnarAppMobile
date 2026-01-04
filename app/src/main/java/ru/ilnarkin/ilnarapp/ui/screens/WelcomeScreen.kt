dc852e04ed837cc8b46371158dbdfd84590e5990
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-04 13:10:54 +0300

    Implemented network error handling and snack bar

--- app/src/main/java/ru/ilnarkin/ilnarapp/ui/screens/WelcomeScreen.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/screens/WelcomeScreen.kt
@@ -1,55 +1,83 @@
 package ru.ilnarkin.ilnarapp.ui.screens
 
-import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
-import kotlinx.coroutines.delay
+import org.koin.androidx.compose.koinViewModel
+import org.koin.compose.koinInject
 import ru.ilnarkin.ilnarapp.R
-import ru.ilnarkin.ilnarapp.helpers.KEY_TOKEN
-import ru.ilnarkin.ilnarapp.helpers.PREFS_NAME
+import ru.ilnarkin.ilnarapp.enums.NetworkErrorType
+import ru.ilnarkin.ilnarapp.network.NetworkErrorManager
 import ru.ilnarkin.ilnarapp.routes.NavRoutes
+import ru.ilnarkin.ilnarapp.viewModels.UserViewModel
 
 
 @Composable
-fun WelcomeScreen(navController: NavController) {
+fun WelcomeScreen(
+	navController: NavController,
+	userViewModel: UserViewModel = koinViewModel(),
+	errorManager: NetworkErrorManager = koinInject()
+) {
 
-	val context = LocalContext.current
+	val snackBarHostState = remember { SnackbarHostState() }
+	val scope = rememberCoroutineScope()
 
-	val sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+	val state by userViewModel.uiState.collectAsState()
 
 
 	LaunchedEffect(Unit) {
-		delay(2000)
 
-		val token = sharedPreferences.getString(KEY_TOKEN, null)
+		userViewModel.checkAuth()
 
-		if (token == null){
-			navController.navigate(NavRoutes.LoginScreen.route){
-				popUpTo(navController.graph.findStartDestination().id) {
-					inclusive = true
-				}
-			}
-		}
-
-		else{
+		if (state.isAuth){
 			navController.navigate(NavRoutes.PinLockScreen.route){
 				popUpTo(navController.graph.findStartDestination().id) {
 					inclusive = true
 				}
 			}
 		}
+
+		errorManager.errorEvent.collect { error ->
+			when(error){
+				NetworkErrorType.NO_INTERNET -> {
+					snackBarHostState.showSnackbar("Проверь интернет соединение", duration = SnackbarDuration.Short)
+				}
+
+				NetworkErrorType.SERVER_ERROR -> {
+					snackBarHostState.showSnackbar("Сервер недоступен", duration = SnackbarDuration.Short)
+				}
+
+				NetworkErrorType.UNAUTHORIZED -> {
+					navController.navigate(NavRoutes.LoginScreen.route){
+						popUpTo(navController.graph.findStartDestination().id) {
+							inclusive = true
+						}
+					}
+				}
+			}
+		}
 	}
+
 
 	Box(modifier = Modifier
 		.fillMaxSize()
@@ -59,5 +87,16 @@ fun WelcomeScreen(navController: NavController) {
 		Image(
 			painter = painterResource(R.drawable.logo),
 			contentDescription = "Logo")
+
+		SnackbarHost(
+			hostState = snackBarHostState,
+			modifier = Modifier.padding(16.dp).align(Alignment.BottomCenter)
+		){data ->
+			Snackbar(
+				snackbarData = data,
+				containerColor = Color.White,
+				contentColor = colorResource(R.color.text_color)
+			)
+		}
 	}
 }
