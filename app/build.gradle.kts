e26c3b71bc196ff7f7feb5f456d6c279d16332e9
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-04 13:05:41 +0300

    Installed logging interceptor dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation (libs.koin.android)
     implementation (libs.koin.androidx.navigation)
     implementation (libs.koin.androidx.compose)
+    implementation (libs.logging.interceptor)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
