2ad903715b6d7ee7cc711b82f1686a816d236ee8
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-04 13:08:03 +0300

    Refactoring

--- app/src/main/java/ru/ilnarkin/ilnarapp/interceptors/NetworkErrorInterceptor.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/interceptors/NetworkErrorInterceptor.kt
@@ -1,17 +1,27 @@
 package ru.ilnarkin.ilnarapp.interceptors
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
 import ru.ilnarkin.ilnarapp.enums.NetworkErrorType
 import ru.ilnarkin.ilnarapp.network.NetworkErrorManager
 import java.io.IOException
+import java.net.ConnectException
+import java.net.SocketTimeoutException
 
 
-class NetworkErrorInterceptor(private val errorManager: NetworkErrorManager) : Interceptor {
+class NetworkErrorInterceptor(private val errorManager: NetworkErrorManager, private val context: Context) : Interceptor {
 	override fun intercept(chain: Interceptor.Chain): Response {
+		val response = chain.proceed(chain.request())
+
 		try {
-			val response = chain.proceed(chain.request())
+
+			if (!response.isSuccessful) {
+				runBlocking { errorManager.notifyError(NetworkErrorType.NO_INTERNET) }
+			}
 
 			when (response.code){
 				502 -> runBlocking { errorManager.notifyError(NetworkErrorType.SERVER_ERROR) }
@@ -19,11 +29,24 @@ class NetworkErrorInterceptor(private val errorManager: NetworkErrorManager) : I
 				401 -> runBlocking { errorManager.notifyError(NetworkErrorType.UNAUTHORIZED) }
 			}
 
-			return response
+		}
 
-		} catch (e: IOException) {
+		catch (e: ConnectException) {
 			runBlocking { errorManager.notifyError(NetworkErrorType.NO_INTERNET) }
 			throw e
 		}
+
+		catch (e: SocketTimeoutException) {
+			runBlocking { errorManager.notifyError(NetworkErrorType.SERVER_ERROR) }
+			throw e
+
+		}
+
+		catch (e: IOException) {
+			runBlocking { errorManager.notifyError(NetworkErrorType.NO_INTERNET) }
+			throw e
+		}
+
+		return response
 	}
 }
--- app/src/main/java/ru/ilnarkin/ilnarapp/network/NetworkErrorManager.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/network/NetworkErrorManager.kt
@@ -1,13 +1,13 @@
 package ru.ilnarkin.ilnarapp.network
 
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import ru.ilnarkin.ilnarapp.enums.NetworkErrorType
 
 
 class NetworkErrorManager {
-	private val _errorEvent = MutableSharedFlow<NetworkErrorType>()
-	val errorEvent = _errorEvent.asSharedFlow()
+	private val _errorEvent = MutableStateFlow(NetworkErrorType.SERVER_ERROR)
+	val errorEvent = _errorEvent.asStateFlow()
 
 	suspend fun notifyError(errorType: NetworkErrorType) {
 		_errorEvent.emit(errorType)
