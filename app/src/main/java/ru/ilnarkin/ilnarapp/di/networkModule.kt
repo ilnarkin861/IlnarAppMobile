548bc59adc02beb1ac223c67bca02055a8e366b5
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-04 13:09:19 +0300

    Registered interceptors and user http service

--- app/src/main/java/ru/ilnarkin/ilnarapp/di/networkModule.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/di/networkModule.kt
@@ -1,6 +1,8 @@
 package ru.ilnarkin.ilnarapp.di
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -8,19 +10,22 @@ import ru.ilnarkin.ilnarapp.helpers.API_URL
 import ru.ilnarkin.ilnarapp.interceptors.AuthInterceptor
 import ru.ilnarkin.ilnarapp.interceptors.NetworkErrorInterceptor
 import ru.ilnarkin.ilnarapp.network.NetworkErrorManager
+import ru.ilnarkin.ilnarapp.network.UserHttpService
+
 
 val networkModule = module {
 
 	single { NetworkErrorManager() }
 
-	single { NetworkErrorInterceptor(get()) }
+	single { NetworkErrorInterceptor(get(), androidContext().applicationContext) }
 
 	single { AuthInterceptor(get()) }
 
 	single {
 		OkHttpClient.Builder()
-			.addInterceptor(get<NetworkErrorInterceptor>())
+			.addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
 			.addInterceptor(get<AuthInterceptor>())
+			.addInterceptor(get<NetworkErrorInterceptor>())
 			.build()
 	}
 
@@ -31,4 +36,6 @@ val networkModule = module {
 			.addConverterFactory(GsonConverterFactory.create())
 			.build()
 	}
+
+	single { get<Retrofit>().create(UserHttpService::class.java) }
 }
