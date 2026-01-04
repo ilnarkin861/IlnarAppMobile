ca11623e623473dcaec419c25523c371d5d00dba
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-04 13:17:14 +0300

    Optimized imports

--- app/src/main/java/ru/ilnarkin/ilnarapp/interceptors/NetworkErrorInterceptor.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/interceptors/NetworkErrorInterceptor.kt
@@ -1,8 +1,6 @@
 package ru.ilnarkin.ilnarapp.interceptors
 
 import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
