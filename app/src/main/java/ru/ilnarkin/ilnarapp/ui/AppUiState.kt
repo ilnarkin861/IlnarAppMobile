2242dbe5cf8d675d772eae3af6605a3e5e9f13e8
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-12 09:39:56 +0300

    Added fields

--- app/src/main/java/ru/ilnarkin/ilnarapp/ui/AppUiState.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/AppUiState.kt
@@ -1,8 +1,15 @@
 package ru.ilnarkin.ilnarapp.ui
 
+import ru.ilnarkin.ilnarapp.models.Pagination
+
 
 data class AppUiState<T> (
 	var success: Boolean = false,
 	var isAuth: Boolean = false,
+	var loading: Boolean = false,
 	var data: T? = null,
+	var offset: Int = 0,
+	var list: MutableList<T> = mutableListOf(),
+	var pagination: Pagination? = null,
+	var userToken: String? = null
 )
