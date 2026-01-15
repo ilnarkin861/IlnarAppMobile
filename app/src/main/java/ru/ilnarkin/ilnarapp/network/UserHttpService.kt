525f2e552bb67238c6a919a529fd7325714d3b36
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-15 14:48:09 +0300

    Implemented user login functional

--- app/src/main/java/ru/ilnarkin/ilnarapp/network/UserHttpService.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/network/UserHttpService.kt
@@ -1,11 +1,19 @@
 package ru.ilnarkin.ilnarapp.network
 
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import ru.ilnarkin.ilnarapp.models.Token
+import ru.ilnarkin.ilnarapp.models.UserAuthData
 
 
 interface UserHttpService {
 
 	@GET("user/auth-check")
 	suspend fun checkAuth() : Response<Any>
+
+
+	@POST("user/login")
+	suspend fun login(@Body userAuthData : UserAuthData) : Response<Token>
 }
