635bce18d706d1716bc0ebeb629d612086e30c7e
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-15 14:41:37 +0300

    Injection

--- app/src/main/java/ru/ilnarkin/ilnarapp/di/appModule.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/di/appModule.kt
@@ -14,6 +14,6 @@ val appModule = module {
 		androidContext().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
 	}
 
-	viewModel { UserViewModel(get()) }
+	viewModel { UserViewModel(get(), get() ) }
 
 }
