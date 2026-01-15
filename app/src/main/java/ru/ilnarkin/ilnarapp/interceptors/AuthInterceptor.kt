6dbf2e4131d9e70a3219be53979ba6f09be72268
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-15 14:44:11 +0300

    Refactoring

--- app/src/main/java/ru/ilnarkin/ilnarapp/interceptors/AuthInterceptor.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/interceptors/AuthInterceptor.kt
@@ -1,21 +1,23 @@
 package ru.ilnarkin.ilnarapp.interceptors
 
-import android.content.SharedPreferences
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
-import ru.ilnarkin.ilnarapp.helpers.KEY_TOKEN
+import ru.ilnarkin.ilnarapp.network.TokenManager
 
 
-class AuthInterceptor(private val prefs: SharedPreferences) : Interceptor {
+class AuthInterceptor(private val tokenManager: TokenManager) : Interceptor {
 	override fun intercept(chain: Interceptor.Chain): Response {
-		val originalRequest = chain.request()
 
-		val token = prefs.getString(KEY_TOKEN, null)
+		val requestBuilder = chain.request().newBuilder()
 
-		val requestBuilder = originalRequest.newBuilder()
+		runBlocking {
 
-		if (token != null) {
-			requestBuilder.addHeader("Authorization", "Bearer $token")
+			val token = tokenManager.getAuthToken()
+
+			if (!token.isNullOrEmpty()){
+				requestBuilder.addHeader("Authorization", "Bearer $token")
+			}
 		}
 
 		return chain.proceed(requestBuilder.build())
