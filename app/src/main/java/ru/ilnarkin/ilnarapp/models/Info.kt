e29407f1aae7d7ffd540b03584bdf7a8dcfe3571
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-16 16:43:24 +0300

    Refactoring

---
+++ app/src/main/java/ru/ilnarkin/ilnarapp/models/Info.kt
@@ -0,0 +1,4 @@
+package ru.ilnarkin.ilnarapp.models
+
+
+data class Info (var success: Boolean, var messages: MutableList<String>)
--- app/src/main/java/ru/ilnarkin/ilnarapp/models/Response.kt
+++
@@ -1,4 +0,0 @@
-package ru.ilnarkin.ilnarapp.models
-
-
-data class Response (var success: Boolean, var messages: MutableList<String>)
