c9fb54ddcc9ca8d9af1e6ea605b3077271e24a9e
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-19 10:06:33 +0300

    Implemented tag repository

---
+++ app/src/main/java/ru/ilnarkin/ilnarapp/repositories/TagRepository.kt
@@ -0,0 +1,34 @@
+package ru.ilnarkin.ilnarapp.repositories
+
+import retrofit2.Response
+import ru.ilnarkin.ilnarapp.models.AppPagination
+import ru.ilnarkin.ilnarapp.models.FilterModel
+import ru.ilnarkin.ilnarapp.models.Tag
+import ru.ilnarkin.ilnarapp.network.TagHttpService
+
+
+class TagRepository(private val httpService: TagHttpService) : Repository<Tag, AppPagination<Tag>> {
+	override suspend fun getList(
+		offset: Int,
+		limit: Int,
+		filter: FilterModel?
+	): Response<AppPagination<Tag>> {
+		TODO("Not yet implemented")
+	}
+
+	override suspend fun getById(id: String): Response<Tag?> {
+		TODO("Not yet implemented")
+	}
+
+	override suspend fun create(model: Tag): Response<Tag> {
+		TODO("Not yet implemented")
+	}
+
+	override suspend fun update(model: Tag): Response<Tag> {
+		TODO("Not yet implemented")
+	}
+
+	override suspend fun delete(id: String): Response<Any> {
+		TODO("Not yet implemented")
+	}
+}
