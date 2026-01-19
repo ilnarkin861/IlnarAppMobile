b47a884fb916b0bb0be732fa64958446032fe318
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-19 10:07:44 +0300

    Registered tag repository and view model

--- app/src/main/java/ru/ilnarkin/ilnarapp/di/appModule.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/di/appModule.kt
@@ -2,11 +2,15 @@ package ru.ilnarkin.ilnarapp.di
 
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
+import ru.ilnarkin.ilnarapp.repositories.TagRepository
+import ru.ilnarkin.ilnarapp.viewModels.TagViewModel
 import ru.ilnarkin.ilnarapp.viewModels.UserViewModel
 
 
 val appModule = module {
 
 	viewModel { UserViewModel(get(), get() ) }
+	viewModel { TagViewModel(get()) }
 
+	single { TagRepository(get()) }
 }
