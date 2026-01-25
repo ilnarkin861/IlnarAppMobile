a6b93a17674a5883cc4ff9cc058d9b808b98dbf6
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-25 14:02:06 +0300

    Replaced flow

--- app/src/main/java/ru/ilnarkin/ilnarapp/network/NetworkErrorManager.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/network/NetworkErrorManager.kt
@@ -1,13 +1,13 @@
 package ru.ilnarkin.ilnarapp.network
 
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import ru.ilnarkin.ilnarapp.enums.NetworkErrorType
 
 
 class NetworkErrorManager {
-	private val _errorEvent = MutableStateFlow(NetworkErrorType.SERVER_ERROR)
-	val errorEvent = _errorEvent.asStateFlow()
+	private val _errorEvent = MutableSharedFlow<NetworkErrorType>(extraBufferCapacity = 1)
+	val errorEvent = _errorEvent.asSharedFlow()
 
 	suspend fun notifyError(errorType: NetworkErrorType) {
 		_errorEvent.emit(errorType)
