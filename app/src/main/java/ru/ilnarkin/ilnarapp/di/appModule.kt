5019a5dd04b92cc271c2ddb821622c08c17245e8
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-25 13:59:22 +0300

    Injected SharedPreferences

--- app/src/main/java/ru/ilnarkin/ilnarapp/di/appModule.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/di/appModule.kt
@@ -1,13 +1,21 @@
 package ru.ilnarkin.ilnarapp.di
 
+import android.content.Context
+import android.content.SharedPreferences
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
+import ru.ilnarkin.ilnarapp.helpers.PREFS_NAME
 import ru.ilnarkin.ilnarapp.repositories.TagRepository
 import ru.ilnarkin.ilnarapp.viewModels.TagViewModel
 import ru.ilnarkin.ilnarapp.viewModels.UserViewModel
 
 
 val appModule = module {
+
+	single<SharedPreferences> {
+		androidContext().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+	}
 
 	viewModel { UserViewModel(get(), get() ) }
 	viewModel { TagViewModel(get()) }
