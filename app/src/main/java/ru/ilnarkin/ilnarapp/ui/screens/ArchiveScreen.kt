3b7eb661fa6ba4ec02fa1701de199d7f97b55b86
ilnarkin861 <ilnarkino@yandex.ru>
2026-01-28 17:37:54 +0300

    Implemented CRUD operations

--- app/src/main/java/ru/ilnarkin/ilnarapp/ui/screens/ArchiveScreen.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/screens/ArchiveScreen.kt
@@ -1,5 +1,6 @@
 package ru.ilnarkin.ilnarapp.ui.screens
 
+import android.content.Intent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -17,15 +18,22 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
@@ -33,115 +41,173 @@ import androidx.compose.ui.unit.dp
 import com.vanpra.composematerialdialogs.MaterialDialog
 import com.vanpra.composematerialdialogs.MaterialDialogState
 import com.vanpra.composematerialdialogs.rememberMaterialDialogState
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import org.koin.androidx.compose.koinViewModel
+import org.koin.compose.koinInject
 import ru.ilnarkin.ilnarapp.R
+import ru.ilnarkin.ilnarapp.WelcomeActivity
 import ru.ilnarkin.ilnarapp.enums.ActionType
+import ru.ilnarkin.ilnarapp.enums.NetworkErrorType
+import ru.ilnarkin.ilnarapp.helpers.NO_INTERNET_ERROR_MESSAGE
+import ru.ilnarkin.ilnarapp.helpers.SERVER_ERROR_MESSAGE
+import ru.ilnarkin.ilnarapp.models.Archive
+import ru.ilnarkin.ilnarapp.network.NetworkErrorManager
 import ru.ilnarkin.ilnarapp.ui.components.AlertComponent
 import ru.ilnarkin.ilnarapp.ui.components.ItemFormComponent
 import ru.ilnarkin.ilnarapp.ui.components.ListItemComponent
 import ru.ilnarkin.ilnarapp.ui.components.LoadButtonComponent
 import ru.ilnarkin.ilnarapp.ui.components.MessageComponent
 import ru.ilnarkin.ilnarapp.ui.components.ProgressIndicatorComponent
+import ru.ilnarkin.ilnarapp.viewModels.ArchiveViewModel
 
 
 @Composable
-fun ArchiveScreen() {
+fun ArchiveScreen(
+	archiveViewModel: ArchiveViewModel = koinViewModel(),
+	errorManager: NetworkErrorManager = koinInject()
+) {
 
-	val archives = getArchives(20)
+	val limit = 10
 
-	var loading by remember { mutableStateOf(false) }
+	val context = LocalContext.current
 
 	val listState = rememberLazyListState()
-
-	val itemText = remember { mutableStateOf("") }
-
-	val alertTitle = remember { mutableStateOf("") }
 
 	var showAlert by remember { mutableStateOf(false) }
 
 	var modalFormLabel by remember { mutableStateOf("") }
 
-	var success by remember { mutableStateOf(true) }
-
 	val dialogState = rememberMaterialDialogState()
 
 	var actionType by remember { mutableStateOf(ActionType.CREATE) }
 
+	val state by archiveViewModel.uiState.collectAsState()
+
+	val itemId = remember { mutableStateOf("") }
+
+	val itemText = remember { mutableStateOf("") }
+
+	val scope = rememberCoroutineScope()
+
+	val snackBarHostState = remember { SnackbarHostState() }
+
 
 	LaunchedEffect(Unit) {
-		loading = true
 
-		delay(1500)
+		lateinit var errorJob: Job
 
-		loading = false
+		errorJob = launch {
+			errorManager.errorEvent.collect { error ->
+
+				when(error){
+					NetworkErrorType.NO_INTERNET ->
+						snackBarHostState.showSnackbar(NO_INTERNET_ERROR_MESSAGE, duration = SnackbarDuration.Long)
+
+					NetworkErrorType.SERVER_ERROR ->
+						snackBarHostState.showSnackbar(SERVER_ERROR_MESSAGE, duration = SnackbarDuration.Long)
+
+					NetworkErrorType.UNAUTHORIZED -> {
+						val intent = Intent(context, WelcomeActivity::class.java).apply {
+							flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+						}
+
+						context.startActivity(intent)
+
+						errorJob.cancel()
+					}
+				}
+			}
+		}
+
+		archiveViewModel.getArchivesList(state.offset, limit)
 	}
 
 
 	Box(Modifier.fillMaxSize().padding(horizontal = dimensionResource(R.dimen.container_horizontal_padding))) {
 
-		if (loading){
-			Box(
-				modifier = Modifier.fillMaxSize(),
-				contentAlignment = Alignment.Center){
-				ProgressIndicatorComponent(60, colorResource(R.color.primary_color))
-			}
-		}
-
-		if (!loading && !archives.isEmpty()){
+		if (!state.loading && !state.list.isEmpty()){
 			LazyColumn(
 				state = listState,
 				contentPadding = PaddingValues(top = 30.dp, bottom = 80.dp)
 			) {
-				item {
-					Row(Modifier.padding(bottom = 25.dp)) {
-						LoadButtonComponent(nextButton = false, action = { delay(1500) })
+
+				state.pagination?.let {
+					if (it.hasPreviousPage){
+						item {
+							Row(Modifier.padding(bottom = 25.dp)) {
+								LoadButtonComponent(nextButton = false, action = {
+									archiveViewModel.getArchivesList(state.offset - limit, limit, false)
+								})
+							}
+						}
 					}
 				}
 
-				itemsIndexed(archives){ index, tag ->
+				itemsIndexed(state.list){index, tag ->
+
 					Row(Modifier.fillMaxWidth().padding(vertical = 10.dp)) {
 						ListItemComponent(
 							tag.id,
 							tag.title,
 
-							editAction = { tag ->
+							editAction = { text ->
 
-								delay(1500)
+								archiveViewModel.getArchiveById(tag.id)
 
-								actionType = ActionType.UPDATE
-								modalFormLabel = "Изменить архив"
-								itemText.value = tag
-								dialogState.show()
+								if (archiveViewModel.uiState.value.success){
+									actionType = ActionType.UPDATE
+									modalFormLabel = "Изменить архив"
+									archiveViewModel.uiState.value.data?.let { itemId.value = it.id }
+									archiveViewModel.uiState.value.data?.let { itemText.value = it.title }
+									dialogState.show()
+								}
+
+								else{
+									showAlert = true
+								}
 							},
 
-							deleteAction = {
-								delay(1500)
-								alertTitle.value = "Архив успешно удален"
+							deleteAction = {id ->
+
+								archiveViewModel.deleteArchive(id)
+
 								showAlert = true
 							}
 						)
 					}
 
-					if (index != archives.count() -1){
+					if (index != state.list.count() -1){
 						HorizontalDivider(thickness = 1.dp, color = colorResource(R.color.border_color))
 					}
 				}
 
-				item {
-					Row(Modifier.padding(top = 25.dp, bottom = 30.dp)) {
-						LoadButtonComponent(action = {
-							delay(1500)
-							listState.scrollToItem(0)
-						})
+				state.pagination?.let {
+					if (it.hasNextPage){
+						item {
+							Row(Modifier.padding(top = 25.dp, bottom = 30.dp)) {
+								LoadButtonComponent(action = {
+									archiveViewModel.getArchivesList(state.offset + limit, limit, false)
+								})
+							}
+						}
 					}
 				}
 			}
 		}
 
-		if (!loading && archives.isEmpty()){
+		if (!state.loading && state.list.isEmpty()){
 			Box(modifier = Modifier.background(colorResource(R.color.app_bg_color)).fillMaxSize(),
 				contentAlignment = Alignment.Center){
 				MessageComponent("Архивов нет")
+			}
+		}
+
+		if (state.loading){
+			Box(
+				modifier = Modifier.background(colorResource(R.color.app_bg_color)).fillMaxSize(),
+				contentAlignment = Alignment.Center){
+				ProgressIndicatorComponent(60, colorResource(R.color.primary_color))
 			}
 		}
 
@@ -156,25 +222,44 @@ fun ArchiveScreen() {
 				.background(Color.Transparent),
 			onClick = {
 				actionType = ActionType.CREATE
-				modalFormLabel = "Создать архив"
+				modalFormLabel = "Добавить архив"
 				itemText.value = ""
 				dialogState.show()
 			}) {
 			Icon(
 				modifier = Modifier.size(25.dp),
 				painter = painterResource(R.drawable.ic_plus),
-				contentDescription = "Создать") }
+				contentDescription = "Добавить") }
+
+
+		SnackbarHost(
+			hostState = snackBarHostState,
+			modifier = Modifier.padding(16.dp).align(Alignment.BottomCenter)
+		){data ->
+			Snackbar(
+				snackbarData = data,
+				containerColor = colorResource(R.color.primary_color),
+				contentColor = Color.White
+			)
+		}
 	}// Box
 
 
 	AlertComponent(
-		success = success,
-		message = alertTitle.value,
+		success = state.success,
+		message = state.message,
 		showed = showAlert,
 		action = {
 			showAlert = false
 
-			// еще что-то делаем, может
+			if (state.success){
+				scope.launch {
+
+					val offset = if (state.list.size == 1) state.offset - limit else state.offset
+
+					archiveViewModel.getArchivesList(offset, limit)
+				}
+			}
 		}
 	)
 
@@ -187,23 +272,16 @@ fun ArchiveScreen() {
 		ItemFormComponent(
 			itemText.value,
 			modalFormLabel,
-
-			action = {
-
-				delay(1000)
+			action = {text->
 
 				if (actionType == ActionType.CREATE){
 
-					// Save to db
-
-					alertTitle.value = "Архив успешно создан"
+					archiveViewModel.createArchive(Archive(title = text))
 				}
 
-				else{
+				if (actionType == ActionType.UPDATE){
 
-					// Save to db
-
-					alertTitle.value = "Архив успешно изменен"
+					archiveViewModel.updateArchive(Archive(id = itemId.value, title = text))
 				}
 
 				showAlert = true
