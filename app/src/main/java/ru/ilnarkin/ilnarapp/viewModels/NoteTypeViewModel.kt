d5e9a61b7fa06708a9a3406f84366256ba0cf45a
ilnarkin861 <ilnarkino@yandex.ru>
2026-02-24 17:37:38 +0300

    Refactoring

--- app/src/main/java/ru/ilnarkin/ilnarapp/viewModels/NoteTypeViewModel.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/viewModels/NoteTypeViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import ru.ilnarkin.ilnarapp.helpers.DEFAULT_ERROR_MESSAGE
 import ru.ilnarkin.ilnarapp.models.AppPagination
 import ru.ilnarkin.ilnarapp.models.NoteType
 import ru.ilnarkin.ilnarapp.repositories.NoteTypeRepository
