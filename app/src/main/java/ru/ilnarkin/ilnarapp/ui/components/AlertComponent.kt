10cc44efb7fbecbb5f1098c0987a51c6121e28bc
ilnarkin861 <ilnarkino@yandex.ru>
2026-03-05 19:39:37 +0300

    Set text align

--- app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/AlertComponent.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/AlertComponent.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.DialogProperties
@@ -81,7 +82,7 @@ fun AlertComponent(
 							text = message,
 							fontSize = 16.sp,
 							fontFamily = font,
-
+							textAlign = TextAlign.Center,
 							color = if (!success) colorResource(R.color.danger_color)
 							else colorResource(R.color.primary_color)
 						)
