0b8f82778e8d4405772224613d75e510e5e18be8
ilnarkin861 <ilnarkino@yandex.ru>
2026-03-06 10:39:57 +0300

    Deleted unused imports

--- app/src/main/java/ru/ilnarkin/ilnarapp/repositories/NoteTypeRepository.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/repositories/NoteTypeRepository.kt
@@ -1,12 +1,8 @@
 package ru.ilnarkin.ilnarapp.repositories
 
 import io.ktor.client.HttpClient
-import retrofit2.Response
 import ru.ilnarkin.ilnarapp.helpers.API_URL
-import ru.ilnarkin.ilnarapp.helpers.ARCHIVES_ENDPOINT
 import ru.ilnarkin.ilnarapp.helpers.NOTE_TYPES_ENDPOINT
-import ru.ilnarkin.ilnarapp.models.AppPagination
-import ru.ilnarkin.ilnarapp.models.NoteType
 
 
 class NoteTypeRepository(httpClient: HttpClient) : BaseRepository(httpClient,"$API_URL/$NOTE_TYPES_ENDPOINT")
