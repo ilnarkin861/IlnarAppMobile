82d938c038752320944fa9f0c963985db3d9729f
ilnarkin861 <ilnarkino@yandex.ru>
2026-04-16 09:29:14 +0300

    Refactoring

--- app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/FileUploadComponent.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/FileUploadComponent.kt
@@ -40,7 +40,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
 import org.koin.androidx.compose.koinViewModel
 import ru.ilnarkin.ilnarapp.R
