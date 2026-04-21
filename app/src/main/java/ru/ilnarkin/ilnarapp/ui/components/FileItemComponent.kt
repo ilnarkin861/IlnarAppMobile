b77f33675adb1cf21c28655246897866cb0d8aba
ilnarkin861 <ilnarkino@yandex.ru>
2026-04-21 11:24:29 +0300

    Added border radius

--- app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/FileItemComponent.kt
+++ app/src/main/java/ru/ilnarkin/ilnarapp/ui/components/FileItemComponent.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
@@ -31,6 +33,7 @@ fun FileItemComponent(
 		modifier = Modifier
 			.padding(4.dp)
 			.aspectRatio(1f)
+			.clip(shape = RoundedCornerShape(10.dp))
 			.combinedClickable(
 				onClick = {
 					onClick()
